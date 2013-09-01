e9f892c1cabcb86c70bfbd8b075ef6d5c4f47c4f
Russel Quianm <russelquiam@hotmail.com>
2013-09-01 14:50:29 +0200

    yaaaaa

--- brincolines.php
+++ brincolines.php
@@ -11,61 +11,97 @@
 			 		<h3>Brincolines</h3>
 			 		<p><i><strong>Estos son nuestros inflables:</strong></i></p>
 			 		<div class="menu_inflables">
+<<<<<<< HEAD
 			 			<p>La renta es por día, dentro de un horario de 8 de la mañana y máximo 12 de la noche.</p>
+=======
+			 			<p>La renta es por día, dentro de un horario de 7 de la mañana y máximo 12 de la noche.</p>
+>>>>>>> b9521ee7ffc2d4f23b627f85215166325f168ab1
 
 			 			<figure class="fondoli-griz">
 			 				<img src="http://www.inflamigos.com.mx/inflables/angry-box.jpg" alt="castillo_aventura_middle" />
 			 				<h4>Angry Box<br />(3 x 4)</h4>
+<<<<<<< HEAD
 			 				<figcaption>Con un estilo moderno, y recomendado para los más peques.</figcaption>
+=======
+			 				<figcaption>Descripción del inflable</figcaption>
+>>>>>>> b9521ee7ffc2d4f23b627f85215166325f168ab1
 			 				<h5>$700.00</h5>
 			 				<a href="http://www.inflamigos.com.mx/inflable.php?inflable=angry-box" class="btn_ir" title="ver imagenes de: Angry Box">Mas detalles</a>
 			 			</figure>
 			 			<figure class="fondoli-griz">
 			 				<img src="http://www.inflamigos.com.mx/inflables/caventura_middle.jpg" alt="castillo_aventura_middle" />
 			 				<h4>Castillo Aventura <br />(6 x 3)</h4>
+<<<<<<< HEAD
 			 				<figcaption>El clásico y popular, para toda ocasión.</figcaption>
+=======
+			 				<figcaption>Descripción del inflable</figcaption>
+>>>>>>> b9521ee7ffc2d4f23b627f85215166325f168ab1
 			 				<h5>$600.00</h5>
 			 				<a href="http://www.inflamigos.com.mx/inflable.php?inflable=castillo-aventura" title="ver imagenes de: Castillo Aventura">Mas detalles</a>
 			 			</figure>
 			 			<figure class="fondoli-griz">
 				 			<img src="http://www.inflamigos.com.mx/inflables/cprincess_middle.jpg" alt="castillo_princess_middle" />
 				 			<h4>Castillo Princess<br />(4 x 3)</h4>
+<<<<<<< HEAD
 				 			<figcaption>Para consentir a las princesitas del hogar.</figcaption>
+=======
+				 			<figcaption>Descripción del inflable</figcaption>
+>>>>>>> b9521ee7ffc2d4f23b627f85215166325f168ab1
 				 			<h5>$600.00</h5>
 				 			<a href="http://www.inflamigos.com.mx/inflable.php?inflable=castillo-princess" title="ver imagenes de: Castillo Princess">Mas detalles</a>
 			 			</figure>
 						<figure class="fondoli-griz">
 						 	<img src="http://www.inflamigos.com.mx/inflables/escaladrilo-middle.jpg" alt="Escaladrilo-middle"/>
 						 	<h4>Escaladrilo<br />(4 x 2.5)</h4>
+<<<<<<< HEAD
 						 	<figcaption>Especial para la alberca y espacios pequeños.</figcaption>
+=======
+						 	<figcaption>Descripción del inflable</figcaption>
+>>>>>>> b9521ee7ffc2d4f23b627f85215166325f168ab1
 						 	<h5>$600.00</h5>
 						 	<a href="http://www.inflamigos.com.mx/inflable.php?inflable=escaladrilo" title="ver imagenes de: Escaladrilo">Mas detalles</a>
 						</figure>
 						<figure class="fondoli-griz">
 							<img src="http://www.inflamigos.com.mx/inflables/bob-esponja-middle.jpg" alt="Bob-Esponja-Resbaladilla-middle"/>
 							<h4>Bob Esponja<br />(7 x 4)</h4>
+<<<<<<< HEAD
 							<figcaption>Atractivos y alegres colores que daran vida a tus fiestas.</figcaption>
+=======
+							<figcaption>Descripción del inflable</figcaption>
+>>>>>>> b9521ee7ffc2d4f23b627f85215166325f168ab1
 							<h5>$1000.00</h5>
 							<a href="http://www.inflamigos.com.mx/inflable.php?inflable=resbaladilla-bob-esponja" title="ver imagenes de: Resbaladilla Bob Esponja">Mas detalles</a>
 						</figure>
 						<figure class="fondoli-griz">
 							<img src="http://www.inflamigos.com.mx/inflables/ucubo_middle.jpg" alt="ultra_cubo_middle" />
 							<h4>Ultra Cubo<br />(7 x 5)</h4>
+<<<<<<< HEAD
 							<figcaption>El gran interactivo inflamigo, acompañado con rótulos de Yoshi y Mario.</figcaption>
+=======
+							<figcaption>Descripción del inflable</figcaption>
+>>>>>>> b9521ee7ffc2d4f23b627f85215166325f168ab1
 							<h5>$1,200.00</h5>
 							<a href="http://www.inflamigos.com.mx/inflable.php?inflable=ultra-cubo" title="ver imagenes de: Ultra Cubo">Mas detalles</a>
 						</figure>
 						<figure class="fondoli-griz">
 							<img src="http://www.inflamigos.com.mx/inflables/aslide_middle.jpg" alt="arco_slide_middle" /> 
 							<h4>Arco Slide<br />(7 x 5) </h4>
+<<<<<<< HEAD
 							<figcaption>Colócalo junto a tu alberca y prepárense para la diversión.</figcaption>
+=======
+							<figcaption>Descripción del inflable</figcaption>
+>>>>>>> b9521ee7ffc2d4f23b627f85215166325f168ab1
 							<h5>$1,200.00</h5>
 							<a href="http://www.inflamigos.com.mx/inflable.php?inflable=arco-slide" title="ver imagenes de: Arco Slide">Mas detalles</a>
 						</figure>
 						<figure class="fondoli-griz">
 							<img src="http://www.inflamigos.com.mx/inflables/space-lab-middle.jpg" alt="Space-Lab-middle"/> 
 							 <h4>Space Lab<br />6 x 6) </h4>
+<<<<<<< HEAD
 							 <figcaption>El laboratorio espacial invadido por aliens.</figcaption>
+=======
+							 <figcaption>Descripción del inflable</figcaption>
+>>>>>>> b9521ee7ffc2d4f23b627f85215166325f168ab1
 							<h5>$1,500.00</h5>
 							<a href="http://www.inflamigos.com.mx/inflable.php?inflable=space-lab" title="ver imagenes de: Space Lab">Mas detalles</a>
 						</figure>
@@ -74,14 +110,22 @@
 						<figure class="fondoli-griz">
 							<img src="http://www.inflamigos.com.mx/inflables/aqualand-middle.jpg" alt="Aqualand-middle" /> 
 							<h4>Aqualand<br />(4.5 x 5)</h4>
+<<<<<<< HEAD
 							<figcaption>Un inflable muy completo, resbaladilla, área para brincar y chapoteadero.</figcaption>
+=======
+							<figcaption>Descripción del inflable</figcaption>
+>>>>>>> b9521ee7ffc2d4f23b627f85215166325f168ab1
 						 	<h5>$1,300.00</h5>
 						 	<a href="http://www.inflamigos.com.mx/inflable.php?inflable=aqualand" title="ver imagenes de: Aqualand">Mas detalles</a>
 						</figure>
 						<figure class="fondoli-griz">
 							<img src="http://www.inflamigos.com.mx/inflables/splash-pool-middle.jpg" alt="Splash-Pool_middle"/> 
 							<h4>Splash Pool<br />(9 x 5)</h4>
+<<<<<<< HEAD
 							<figcaption>El inflamigo extremo, doble tobogán y un refrescante chapoteadero.</figcaption>
+=======
+							<figcaption>Descripción del inflable</figcaption>
+>>>>>>> b9521ee7ffc2d4f23b627f85215166325f168ab1
 							<h5>$1,800.00</h5>
 							<a href="http://www.inflamigos.com.mx/inflable.php?inflable=splash-pool" title="ver imagenes de: Splash Pool">Mas detalles</a>
 						</figure>
